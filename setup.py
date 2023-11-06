9fdeed39a446313ed6baf72ae76f2f08b441cb6b
JohnRushKucharski <johnkucharski@gmail.com>
2023-11-05 15:25:48 -1000

    minor edit to github-release action.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='canteens',
-    version='0.0.7',
+    version='0.0.8',
     description='A water resources reservoirs system model.',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
